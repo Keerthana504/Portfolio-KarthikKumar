243bc6be7c0fe6c42ffb1d467a2c8ef23968ad3e
Keerthana504 <130107749+Keerthana504@users.noreply.github.com>
2024-01-20 09:34:42 -0500

    Karthik Portfolio

--- index.js
+++ index.js
@@ -24,10 +24,40 @@ const closeMenu = () => {
   sideMenu.style.right = `-200px`;
 };
 
+const toggleShowMore = (element, btn) => {
+  if (!element.hasAttribute("clicked")) {
+    element.style.display = "block";
+    element.setAttribute("clicked", true);
+    btn.innerHTML = "Read Less";
+  } else {
+    element.style.display = "none";
+    element.removeAttribute("clicked", true);
+    btn.innerHTML = "Read More";
+  }
+};
+
+const readMoreCloud = () => {
+  let element = document.querySelector("#readMoreCloud");
+  let btn = document.querySelector("#cloud");
+  toggleShowMore(element, btn);
+};
+
+const readMoreDeploy = () => {
+  let element = document.querySelector("#readMoreDeploy");
+  let btn = document.querySelector("#deploy");
+  toggleShowMore(element, btn);
+};
+
+const readMoreSecurity = () => {
+  let element = document.querySelector("#readMoreSecurity");
+  let btn = document.querySelector("#security");
+  toggleShowMore(element, btn);
+};
+
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbxo8aVrLmxDYgt9aGCX0q0bE2Zm_RWhPCE5mpsX_zHIWoKICJNfqCw_WmDeI-HhfZuY/exec";
 const form = document.forms["submit-to-google-sheet"];
-const msg = document.getElementById("msg");
+const msg = document.getElementById("success-msg");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
